7099df3384a7ea403e2eb36dbb0ebc66e11e40a1
Alan0602 <alankanil1234@gmail.com>
2023-06-13 11:55:26 +0530

    completed

--- src/modules/pages/Signup/signupApi.ts
+++ src/modules/pages/Signup/signupApi.ts
@@ -2,7 +2,7 @@ import { AxiosError } from "axios";
 import { privateGateway } from "../../../services/apiGateway";
 import { routes } from "../../../services/routes";
 import { NavigateFunction } from "react-router-dom";
-import { exist, notify, success } from "../../components/Common/Tostify";
+import { exist, success } from "../../components/Common/Tostify";
 
 
 export const signup = async (
